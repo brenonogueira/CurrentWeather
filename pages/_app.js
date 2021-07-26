9383f096c2416cd6307945dfb1cf7b9ace1cd214
Breno Nogueira Araújo <breno.nog.araujo@hotmail.com>
2021-07-26 11:28:00 -0400

    Aplicação funcionando sem estilização

---
+++ pages/_app.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  /* reset css */
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    box-sizing: border-box;
+    font-family: sans-serif;
+    background-color: #D9E6F6;
+  }
+  #_next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+`
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
